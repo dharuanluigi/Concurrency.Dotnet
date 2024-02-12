ad93d9d71bb054276b73050d7332c1e95ec25717
Dharuan Luigi <dharuanluigi@gmail.com>
2024-02-12 16:07:43 -0300

    Added: implementation to get values to perform tests from user input

--- Models/Command.cs
+++ Models/Command.cs
@@ -6,10 +6,32 @@ public class Command
 {
     public ECommandType Type { get; set; }
     
+    public string? UserId { get; private set; }
+
+    public int Loops { get; private set; }
+    
     public Command(string flag)
     {
         if (string.IsNullOrEmpty(flag) || string.IsNullOrWhiteSpace(flag)) throw new ArgumentException("Flag for command should be informed!");
         Type = Categorize(flag);
+    }
+
+    public void SetUserId(string id)
+    {
+        if (string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id)) throw new ArgumentException("User id must be sent");
+        UserId = id;
+    }
+
+    public void SetLoops(string loops)
+    {
+        if (string.IsNullOrEmpty(loops) || string.IsNullOrWhiteSpace(loops)) throw new ArgumentException("Loops should be pass!");
+        try {
+            Loops = int.Parse(loops);
+        }
+        catch (Exception)
+        {
+            throw new ArgumentException("Loop should be a number");
+        }
     }
 
     private static ECommandType Categorize(string flag)
